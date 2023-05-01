1c6778fc5acf017e66a1dedf762f33d90eec4cf9
c5rogers <natenaelhaylemariam@gmail.com>
2023-05-01 07:58:18 -0400

    modified

--- src/redux/musicSaga.js
+++ src/redux/musicSaga.js
@@ -1,9 +1,9 @@
-import { takeEvery, put,all } from 'redux-saga/effects'
-import { SET_MUSIC_LIST, MUSIC_LIST,ADD_MUSIC,APPEND_MUSIC_LIST,EDIT_MUSIC,REPLACE_MUSIC,FILTER_MUSIC,DELETE_MUSIC,SET_MUSIC,GET_MUSIC,SET_WHOLE_EDITED_MUSIC,EDIT_WHOLE_MUSIC} from "./constant";
+import { takeEvery, put, all } from 'redux-saga/effects'
+import { SET_MUSIC_LIST, MUSIC_LIST, ADD_MUSIC, APPEND_MUSIC_LIST, EDIT_MUSIC, REPLACE_MUSIC, FILTER_MUSIC, DELETE_MUSIC, SET_MUSIC, GET_MUSIC, SET_WHOLE_EDITED_MUSIC, EDIT_WHOLE_MUSIC } from "./constant";
 import axios from 'axios';
 
 function* getMusics() {
-    const jsonData = yield axios.get('http://localhost:5000/musics')
+    const jsonData = yield axios.get('https://database-nine-alpha.vercel.app/musics')
     const data = yield jsonData.data
     yield put({
         type: SET_MUSIC_LIST,
@@ -11,56 +11,56 @@ function* getMusics() {
     })
 }
 
-function* getMusic(action){
+function* getMusic(action) {
 
-    const jsonData=yield axios.get(`http://localhost:5000/musics/${action.payload}`)
-    const data=yield jsonData.data
+    const jsonData = yield axios.get(`https://database-nine-alpha.vercel.app/musics/${action.payload}`)
+    const data = yield jsonData.data
     yield put({
-        type:SET_MUSIC,
+        type: SET_MUSIC,
         data
     })
 
 }
 
-function* addMusics(action){
+function* addMusics(action) {
 
 
-    const jsonData=yield axios.post('http://localhost:5000/musics',action.payload)
-    const data=yield jsonData.data
+    const jsonData = yield axios.post('https://database-nine-alpha.vercel.app/musics', action.payload)
+    const data = yield jsonData.data
     yield put({
-        type:APPEND_MUSIC_LIST,
+        type: APPEND_MUSIC_LIST,
         data
     })
 
 }
 
 
-function* editMusic(action){
+function* editMusic(action) {
 
-    const jsonData=yield axios.put(`http://localhost:5000/musics/${action.payload.id}`,action.payload)
-    const data=yield jsonData.data
+    const jsonData = yield axios.put(`https://database-nine-alpha.vercel.app/musics/${action.payload.id}`, action.payload)
+    const data = yield jsonData.data
     yield put({
-        type:REPLACE_MUSIC,
+        type: REPLACE_MUSIC,
         data
     })
 
 }
 
-function* editWholeMusic(action){
-    const jsonData=yield axios.put(`http://localhost:5000/musics/${action.payload.id}`,action.payload)
-    const data=yield jsonData.data
+function* editWholeMusic(action) {
+    const jsonData = yield axios.put(`https://database-nine-alpha.vercel.app/musics/${action.payload.id}`, action.payload)
+    const data = yield jsonData.data
 
     yield put({
-        type:SET_WHOLE_EDITED_MUSIC,
+        type: SET_WHOLE_EDITED_MUSIC,
         data
     })
 }
 
-function* deleteMusic(action){
-    yield axios.delete(`http://localhost:5000/musics/${action.payload}`)
+function* deleteMusic(action) {
+    yield axios.delete(`https://database-nine-alpha.vercel.app/musics/${action.payload}`)
     yield put({
-        type:FILTER_MUSIC,
-        data:action.payload
+        type: FILTER_MUSIC,
+        data: action.payload
     })
 }
 
@@ -71,11 +71,11 @@ function* musicSaga() {
     yield all([
         takeEvery(MUSIC_LIST, getMusics),
         takeEvery(ADD_MUSIC, addMusics),
-        takeEvery(EDIT_MUSIC,editMusic),
-        takeEvery(DELETE_MUSIC,deleteMusic),
-        takeEvery(GET_MUSIC,getMusic),
-        takeEvery(EDIT_WHOLE_MUSIC,editWholeMusic)
-        ])
+        takeEvery(EDIT_MUSIC, editMusic),
+        takeEvery(DELETE_MUSIC, deleteMusic),
+        takeEvery(GET_MUSIC, getMusic),
+        takeEvery(EDIT_WHOLE_MUSIC, editWholeMusic)
+    ])
 
 }
 
